b6c280cefcb0a83a1ebfe7da09f249008bebb998
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-06 07:23:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
 # code your #valid_move? method here
 
 def valid_move?()
+  if position_taken?
+    true
+  elsif !position_taken?
+    false
 end  
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
