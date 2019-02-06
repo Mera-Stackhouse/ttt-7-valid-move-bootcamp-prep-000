4d3a1ab2819b9e639433a57dece42dcd81e2a816
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-06 07:49:45 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 
 def valid_move?(board, index)
@@ -29,5 +29,5 @@ def position_taken?(board, index)
   end  
 end
 
-puts board[0]
-puts position_taken?(board, 0)
+#puts board[0]
+#puts position_taken?(board, 0)
